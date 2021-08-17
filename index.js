f68a11745815076edadd4c9b5bb432933bc06385
Martell Howze <martellhowze042988@gmail.com>
2021-08-16 21:27:54 -0500

    commit tree 420afe8881910fb3c696cd89dbd8bc22a3054f10

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Code your solution in this file!
+const hq = 42;
+
+
+
+function distanceFromHqInBlocks(pickup){
+ if(pickup > hq){
+     return pickup - hq
+    }
+else{
+    return hq - pickup
+}
+
+}
+
+function distanceFromHqInFeet(pickup){
+    return distanceFromHqInBlocks(pickup) * 264
+
+}
+
+function distanceTravelledInFeet(start, stop){
+    if(start > stop){
+        return (start - stop) * 264
+    } else if(start < stop){
+        return (stop - start) * 264
+    }
+
+}
+
+function calculatesFarePrice(start, stop){
+    const totalFeet = distanceTravelledInFeet(start, stop)
+    
+
+    if(totalFeet < 400){
+        return 0
+  
+    }else if((totalFeet > 400) && (totalFeet < 2000)){
+       
+        return (totalFeet - 400) * .02
+   
+    }else if(totalFeet > 2500){
+        return 'cannot travel that far'
+    }else if (totalFeet > 2000){
+        return 25
+    }
+}
